7e84a2cbb43af6227fabb7ae3ee43cc8324ebe6e
Raja Marthala <raja.marthala@fliq.fi>
2020-10-27 11:11:21 +0200

    initial commit

---
+++ Angular course/Udmey Angular - The Complete Guide (2020 edition)/my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  //template: `<app-server></app-server>
+  //<app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No sever created';
+  serverName = 'test';
+  serverCreated = false;
+  constructor() {
+    setTimeout(()=>{this.allowNewServer=true;},3000);
+   }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event:any){
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
+}
